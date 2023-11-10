45e38356c9172c6674681263c2c9fb800e1c8600
Julliane Tampus <julliane.tampus123@gmail.com>
2023-11-10 23:37:43 +0800

    added comment functionality

--- routers/Home.js
+++ routers/Home.js
@@ -10,9 +10,49 @@ router.get('/', (req, res)=>{
     db.query(post , (err , result)=>{
         if(err) return console.log(err)
        // console.log(result)
-        return res.json(result)
+        return res.json(result.rows)
     })
 
 })
+
+
+
+router.post('/comment' , (req, res)=>{
+
+    const data = [ req.body.postid , req.body.userid , req.body.comment]
+
+    const comment = 'INSERT INTO comments(post_id , user_id , user_comment) VALUES($1 , $2 , $3)'
+
+    db.query(comment,data, (error , result)=>{
+        if(error){
+            console.log(error)
+            return res.send({successful : false})
+        }
+        if(result) return res.send({successful : true})
+    })
+})
+
+
+router.get('/comment', (req, res)=>{
+    const post_id = req.query.id
+
+    const post_comment = `SELECT * FROM view_comments WHERE post_id = ${post_id} OFFSET ${req.query.current} LIMIT ${req.query.lastpage}`
+
+    db.query(post_comment , (error , result)=>{
+        if(error){
+            console.log(error)
+            return res.send({successful : false})
+        }
+
+        if(result){
+            return res.send(result.rows)
+        }
+
+    })
+
+
+
+})
+
 
 module.exports = router
