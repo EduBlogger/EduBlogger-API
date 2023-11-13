027ca3fceb99b7d0bfc447ceae37e01d1036593a
Julliane Tampus <julliane.tampus123@gmail.com>
2023-11-13 23:03:10 +0800

    fixed commet bug and added likes-reaction

--- routers/Home.js
+++ routers/Home.js
@@ -9,12 +9,10 @@ router.get('/', (req, res)=>{
 
     db.query(post , (err , result)=>{
         if(err) return console.log(err)
-       // console.log(result)
         return res.json(result.rows)
     })
 
 })
-
 
 
 router.post('/comment' , (req, res)=>{
@@ -49,10 +47,69 @@ router.get('/comment', (req, res)=>{
         }
 
     })
-
-
-
 })
+
+
+router.post('/react', (req, res)=>{
+    const data = {
+        user_id : req.body.user_id,
+        post_id : req.body.post_id,
+    }
+
+
+    const liked = `INSERT INTO liked(post_id , user_id) VALUES(${data.post_id} , ${data.user_id})`
+
+    db.query(liked, (error , result)=>{
+        if(error){
+            console.log(error)
+            return res.send({successful : false})
+        }
+        if(result) return res.send({successful : true})
+    })
+})
+
+router.post('/disreact', (req, res)=>{
+    const data = {
+        user_id : req.body.user_id,
+        post_id : req.body.post_id,
+    }
+
+
+    const liked = `DELETE FROM liked WHERE user_id = ${data.user_id}  AND post_id = ${data.post_id}`
+
+    db.query(liked, (error , result)=>{
+        if(error){
+            console.log(error)
+            return res.send({successful : false})
+        }
+        if(result) return res.send({successful : true})
+    })
+})
+
+
+router.post('/isreact' , ( req, res)=>{
+    const isliked = `
+            SELECT user_id
+            FROM  liked
+            WHERE user_id = ${req.body.user_id} AND post_id = ${req.body.post_id}
+        `
+
+    db.query(isliked , (error , result)=>{
+        if(error){
+            console.log(error)
+            return res.send({successful : false})
+        }
+
+        if(result.rowCount === 1){
+            return res.send({successful : true})
+        }else{
+            return res.send({successful : false})
+        }
+
+    })
+    
+})
+
 
 
 module.exports = router
